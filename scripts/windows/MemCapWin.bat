1d5edceb42143601a4d84a87260d21bb49895732
Mitch <mitchclifford123@hotmail.com>
2025-11-06 13:04:41 +1100

    Fix WinPmem output error

--- scripts/windows/MemCapWin.bat
+++ scripts/windows/MemCapWin.bat
@@ -1,3 +1,8 @@
-if not exist "..\..\output\WinPmem" mkdir "..\..\output\WinPmem"
-set name = "Please specify output file name: "
-tools\windows\winpmem.exe ..\..\output\WinPmem\%name%
+@echo off
+set TOOLKIT_ROOT=%~dp0..\..
+set OUTPUT_DIR=%TOOLKIT_ROOT%\output\WinPmem
+if not exist "%OUTPUT_DIR%" mkdir "%OUTPUT_DIR%"
+set TIMESTAMP=%date:~-4,4%%date:~-7,2%%date:~-10,2%_%time:~0,2%%time:~3,2%%time:~6,2%
+set TIMESTAMP=%TIMESTAMP: =0%
+"%TOOLKIT_ROOT%\tools\windows\winpmem.exe" "%OUTPUT_DIR%\memory_dump_%TIMESTAMP%.raw"
+cls
