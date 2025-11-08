c97c80e83fc436aa950cf4a4e16494461f4dd983
Mitch <mitchclifford123@hotmail.com>
2025-11-08 16:36:18 +1100

    added an echo to show that checksum is being completed (as it can take time)

--- scripts/windows/checksumgenerator.bat
+++ scripts/windows/checksumgenerator.bat
@@ -6,19 +6,25 @@ if errorlevel 1 (
     echo Failed to change to output directory
     exit /b 1
 )
-
+echo Completing checksum before exiting, please wait...
 for /R "%CD%" %%F in (*) do (
-    set "filepath=%%F"
-    set "outputdir=%CD%"
-    if "!filepath:%outputdir%=!" neq "!filepath!" (
-        if /i not "%%~xF"==".sha256" (
-            certutil -hashfile "%%F" SHA256 | findstr /v "certutil:" | findstr /v "sha256 hash of" > "%%F.sha256"
-            (for /f "usebackq delims=" %%A in ("%%F.sha256") do (
-                echo %%A %%~nxF
-            )) > "%%F.temp"
-            move /y "%%F.temp" "%%F.sha256" > nul
+    if exist "%%F" (
+        if not exist "%%F\" (
+            set "filepath=%%F"
+            set "outputdir=%CD%"
+            if "!filepath:%outputdir%=!" neq "!filepath!" (
+                if /i not "%%~xF"==".sha256" (
+                    certutil -hashfile "%%F" SHA256 | findstr /v "certutil:" | findstr /v "sha256 hash of" > "%%F.sha256"
+                    (for /f "usebackq delims=" %%A in ("%%F.sha256") do (
+                        echo %%A %%~nxF
+                    )) > "%%F.temp"
+                    move /y "%%F.temp" "%%F.sha256" > nul
+                )
+            )
         )
     )
 )
 
 popd
+cls
+exit /b 0
